9f60654ab53d5bd03ec5470923071529f30625a4
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-08-21 19:41:17 -0300

    add calculateTotal action

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import Navbar from "./components/navbar";
 import CartContainer from './components/cartContainer';
-
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 
 function App() {
+  const {cartItems} = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals())
+  },[cartItems])
+
   return <main>
     <Navbar />
     <CartContainer />
--- src/components/cartContainer.js
+++ src/components/cartContainer.js
@@ -13,7 +13,7 @@ const CartContainer = () => {
       </header>
     </section>
   }
-  
+
   return (
     <section className="cart">
       <header>
@@ -27,7 +27,7 @@ const CartContainer = () => {
       <footer>
         <hr/>
         <div className="cart-total">
-          <h4>total <span>${total}</span></h4>
+          <h4>total <span>${total.toFixed(2)}</span></h4>
         </div>
         <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
       </footer>
